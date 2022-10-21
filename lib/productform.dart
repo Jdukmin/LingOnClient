5d2c3a127a05feb5beb92194cdf0ca975639baf8
Jdukmin <zzttit@yonsei.ac.kr>
2022-10-21 13:23:18 +0900

    LingOnClient Push

---
+++ lib/productform.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PayFormRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      
+      // body is the majority of the screen.
+      body: Center(
+
+      ),
+
+      drawer: Drawer(
+      ),
+    );
+  }
+}
--- lib/signin.dart
+++ lib/signin.dart
@@ -17,40 +17,42 @@ class SigninRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container( 
-          color: Colors.white,
-          child: Center( 
-            child: ElevatedButton( 
-              child: Text("카카오 로그인"),
-              onPressed: () async {
-                  if (await isKakaoTalkInstalled()) {
-                    try {
-                      await UserApi.instance.loginWithKakaoTalk();
-                      print('카카오톡으로 로그인 성공');
-                      _get_user_info();
-                    } catch (error) {
-                      print('카카오톡으로 로그인 실패 $error');
-                      // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                      try {
-                        await UserApi.instance.loginWithKakaoAccount();
-                        print('카카오계정으로 로그인 성공');
-                        _get_user_info();
-                      } catch (error) {
-                        print('카카오계정으로 로그인 실패 $error');
-                      }
-                    }
-                  } else {
-                    try {
-                      await UserApi.instance.loginWithKakaoAccount();
-                      print('카카오계정으로 로그인 성공');
-                      _get_user_info();
-                    } catch (error) {
-                      print('카카오계정으로 로그인 실패 $error');
-                    }
+        color: Colors.white,
+        child: Center( 
+          child: ElevatedButton( 
+            child: Text("카카오 로그인"),
+            onPressed: () async {
+              print("Login Button Pressed.");
+              if (await isKakaoTalkInstalled()) {
+                try {
+                  await UserApi.instance.loginWithKakaoTalk();
+                  print('카카오톡으로 로그인 성공');
+                  _get_user_info();
+                } catch (error) {
+                  print('카카오톡으로 로그인 실패 $error');
+                  // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+                  try {
+                    await UserApi.instance.loginWithKakaoAccount();
+                    print('카카오계정으로 로그인 성공');
+                    _get_user_info();
+                  } catch (error) {
+                    print('카카오계정으로 로그인 실패 $error');
                   }
                 }
-              )
-            )
+              }
+              else {
+                try {
+                  await UserApi.instance.loginWithKakaoAccount();
+                  print('카카오계정으로 로그인 성공');
+                  _get_user_info();
+                } catch (error) {
+                  print('카카오계정으로 로그인 실패 $error');
+                }
+              }
+            }
           )
+        )
+      )
     );
   }
 }
